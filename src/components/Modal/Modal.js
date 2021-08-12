ba98ee8936ea1f1ba9c8fc7a07e5a8075e7b5755
esaleh15 <elisaleh1@hotmail.co.uk>
2021-08-12 16:13:14 +0100

    adding modal to form

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import classes from './Modal.module.css';
+
+const Modal = (props) => {
+
+	return (
+		<div>
+			<div className={classes.backdrop} onClick={props.onConfirm}></div>
+			<div className={classes.modal}>
+				<header className={classes.header}>
+					<h2>{props.title}</h2>
+				</header>
+				<div className={classes.content}>
+					<p>{props.message}</p>
+				</div>
+				<footer className={classes.actions}>
+					<button type='button' onClick={props.onConfirm}>Okay</button>
+				</footer>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
--- src/components/User/UserForm/UserForm.js
+++ src/components/User/UserForm/UserForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import Modal from '../../Modal/Modal';
 import classes from './UserForm.module.css';
 
 const UserForm = (props) => {
 
 	const [enteredUsername, setEnteredUsername] = useState('');
 	const [enteredAge, setEnteredAge] = useState('');
+	const [error, setError] = useState();
 
 	const userNameChangeHandler = (event) => {
 		setEnteredUsername(event.target.value);
@@ -17,10 +19,18 @@ const UserForm = (props) => {
 	const submitHandler = (event) => {
 		event.preventDefault();
 		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			setError({
+				title: 'Invalid Input', 
+				message: 'Please enter valid name and age'
+			});
 			return;
 		}
 
 		if (+enteredAge < 1) {
+			setError({
+				title: 'Invalid Age', 
+				message: 'Please enter valid age (> 0)'
+			});
 			return;
 		}
 		const userData = {
@@ -32,9 +42,14 @@ const UserForm = (props) => {
 		setEnteredAge('');
 	};
 
+	const errorHandler = () => {
+		setError(null);
+	};
+
 
 	return (
 			<div>
+			{error && <Modal title={error.title} message={error.message} onConfirm={errorHandler}></Modal>}
 				<form onSubmit={submitHandler}>
 					<div className={`${classes['user-form__container']}`}>
 						<div className={`${classes['user-form__text-input']}`}>
