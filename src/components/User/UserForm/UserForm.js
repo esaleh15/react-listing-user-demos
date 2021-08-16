19a369eeafb1f3263606d351a92d9bbbec29626e
esaleh15 <elisaleh1@hotmail.co.uk>
2021-08-16 13:10:26 +0100

    switch to ref instead of state management

--- src/components/User/UserForm/UserForm.js
+++ src/components/User/UserForm/UserForm.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Modal from '../../Modal/Modal';
 import Wrapper from '../../Helpers/Wrappers/Wrapper';
 import classes from './UserForm.module.css';
 
 const UserForm = (props) => {
 
-	const [enteredUsername, setEnteredUsername] = useState('');
-	const [enteredAge, setEnteredAge] = useState('');
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
+	// const [enteredUsername, setEnteredUsername] = useState('');
+	// const [enteredAge, setEnteredAge] = useState('');
 	const [error, setError] = useState();
 
-	const userNameChangeHandler = (event) => {
-		setEnteredUsername(event.target.value);
-	};
+	// const userNameChangeHandler = (event) => {
+	// 	setEnteredUsername(event.target.value);
+	// };
 
-	const ageChangeHandler = (event) => {
-		setEnteredAge(event.target.value);
-	};
+	// const ageChangeHandler = (event) => {
+	// 	setEnteredAge(event.target.value);
+	// };
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+		const enteredName = nameInputRef.current.value;
+		const enteredUserAge = ageInputRef.current.value;
+		if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
 			setError({
 				title: 'Invalid Input', 
 				message: 'Please enter valid name and age'
@@ -27,7 +31,7 @@ const UserForm = (props) => {
 			return;
 		}
 
-		if (+enteredAge < 1) {
+		if (+enteredUserAge < 1) {
 			setError({
 				title: 'Invalid Age', 
 				message: 'Please enter valid age (> 0)'
@@ -35,12 +39,14 @@ const UserForm = (props) => {
 			return;
 		}
 		const userData = {
-			userName: enteredUsername,
-			age: enteredAge
+			userName: enteredName,
+			age: enteredUserAge
 		};
 		props.onSaveUserData(userData);
-		setEnteredUsername('');
-		setEnteredAge('');
+		nameInputRef.current.value = '';
+		ageInputRef.current.value = ''; // Should not do that - states vs refs
+		// setEnteredUsername('');
+		// setEnteredAge('');
 	};
 
 	const errorHandler = () => {
@@ -56,12 +62,12 @@ const UserForm = (props) => {
 						<div className={`${classes['user-form__text-input']}`}>
 							<label>User Name</label>
 							<br/>
-							<input type="text" value={enteredUsername} onChange={userNameChangeHandler}/>
+							<input id='username' type="text" ref={nameInputRef}/>
 						</div>
 						<div className={`${classes['user-form__text-input']}`}>
 							<label>Age (Years)</label>
 							<br/>
-							<input type="number" value={enteredAge} onChange={ageChangeHandler}/>
+							<input id='age' type="number" ref={ageInputRef}/>
 						</div>
 						<div className={`${classes['user-form']}`}>
 							<button type='submit'>Add User</button>
